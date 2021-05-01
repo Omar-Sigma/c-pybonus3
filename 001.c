7a19fbec29adb31e01d764fc20fe6bd1e584d590
Omar-Sigma <omarzero5000009@gmail.com>
2021-05-01 03:27:50 +0200

    Finished: 01

--- 001.c
+++ 001.c
@@ -15,35 +15,35 @@ void splitter(int n, int *j, int ln2f[]){
 	char ln1s[2*n];
 	fgetc(stdin);
 	fgets(ln1, 2*n, stdin);
-	printf("ln1: %s\n", ln1);
-	printf("i=%d\n", i);
+	//printf("ln1: %s\n", ln1);
+	//printf("i=%d\n", i);
 	strcpy(ln1s, ln1);
 	char *pr=strtok(ln1s, ",");
   while (pr != NULL){
-		printf("i-b=%d\n",i);
-		printf("pr-b:%s\n",pr);
+		//printf("i-b=%d\n",i);
+		//printf("pr-b:%s\n",pr);
 		ln2[i] = atoi(pr);
 		pr=strtok(NULL, ",");
-		printf("i-a=%d\n",i);
-		printf("pr-a:%s\n",pr);
-		printf("%d\n==============\n",ln2[i]);
+		//printf("i-a=%d\n",i);
+		//printf("pr-a:%s\n",pr);
+		//printf("%d\n==============\n",ln2[i]);
 		i++;
 
 	}
 	*j=i;
 	//printf("i=%d\n", i);
-	for (i=0; i<*j; i++){
+	/*for (i=0; i<*j; i++){
 		printf("ln1: %s\n", ln1);
 		printf("ln2: %d\n\n", ln2[i]);
-	}
+	}*/
 	i=0;
 	int szoa0=sizeof(ln2)/sizeof(ln2[0]);
-	printf("\nsome elements of ln2: %d\n", ln2[2]);
+	//printf("\nsome elements of ln2: %d\n", ln2[2]);
 	for (i=0; i<szoa0; i++){
 		ln2f[i]=ln2[i];
-		printf("Element: %d \n", ln2f[i]);
+		//printf("Element: %d \n", ln2f[i]);
 	}
-	printf("Final: %d\n", ln2f[1]);
+	//printf("Final: %d\n", ln2f[1]);
 }
 
 //Splitter: End//
@@ -110,8 +110,8 @@ int main(){
 	printf("Enter orange sizes (Must be less than 10). Seperate with a comma, don't put a '.':\n");
 	int ln2f[]={1,2,3};
 	splitter(n, &j, ln2f);
-	printf("j-final: %d\n", j);
-	printf("%d\n", ln2f[1]);
+	//printf("j-final: %d\n", j);
+	//printf("%d\n", ln2f[1]);
 	Discriminant(j, n, b, d, sum, ln2f);
 	return 0;
 	/*for(i=0, i<sizeof(ln2)+1, i++){
