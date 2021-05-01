4d9281c0380fbfe547f788469421854eeefce20b
Omar-Sigma <omarzero5000009@gmail.com>
2021-05-01 03:22:40 +0200

    Finished, with some stuff used for debugging in there.

--- 001.c
+++ 001.c
@@ -9,8 +9,8 @@ int i=0;
 //Splitter Function//
 //==================//
 
-void splitter(int n, int *j, char* ln2f[]){
-	char* ln2[n];
+void splitter(int n, int *j, int ln2f[]){
+	int ln2[n];
 	char ln1[2*n];
 	char ln1s[2*n];
 	fgetc(stdin);
@@ -22,11 +22,11 @@ void splitter(int n, int *j, char* ln2f[]){
   while (pr != NULL){
 		printf("i-b=%d\n",i);
 		printf("pr-b:%s\n",pr);
-		ln2[i] = pr;
+		ln2[i] = atoi(pr);
 		pr=strtok(NULL, ",");
 		printf("i-a=%d\n",i);
 		printf("pr-a:%s\n",pr);
-		printf("%s\n==============\n",ln2[i]);
+		printf("%d\n==============\n",ln2[i]);
 		i++;
 
 	}
@@ -34,16 +34,16 @@ void splitter(int n, int *j, char* ln2f[]){
 	//printf("i=%d\n", i);
 	for (i=0; i<*j; i++){
 		printf("ln1: %s\n", ln1);
-		printf("ln2: %s\n\n", ln2[i]);
+		printf("ln2: %d\n\n", ln2[i]);
 	}
 	i=0;
 	int szoa0=sizeof(ln2)/sizeof(ln2[0]);
-	printf("\nsome elements of ln2: %s\n", ln2[2]);
+	printf("\nsome elements of ln2: %d\n", ln2[2]);
 	for (i=0; i<szoa0; i++){
 		ln2f[i]=ln2[i];
-		printf("Element: %s \n", ln2f[i]);
+		printf("Element: %d \n", ln2f[i]);
 	}
-	printf("Final: %s\n", ln2f[1]);
+	printf("Final: %d\n", ln2f[1]);
 }
 
 //Splitter: End//
@@ -54,35 +54,40 @@ void splitter(int n, int *j, char* ln2f[]){
 //Discriminant Function//
 //==================//
 
-int Discriminant(int j, int n, int b, int d, int sum, char* ln2){
+void Discriminant(int j, int n, int b, int d, int sum, int ln2f[]){
 	int cl=0;
 	int l=0;
+  
 	//First Level
 	for (i=0; i<j; i++){
 		//Second Level
 		if (cl == 0){
 			//Third Level
 			
-			if (1 <= n && n<= 100000 && 1 <= b && b <= d && b<= 1000000 && 1 <= ln2[i]&& ln2[i] <= 1000000){
+			if (1 <= n && n<= 100000 && 1 <= b && b <= d && b<= 1000000 && 1 <= ln2f[i] && ln2f[i] <= 1000000){
 				//Fourth Level
 				for (l=0; l<j; l++){
 					//Fifth Level
-					if (ln2[l] <= b){
-						sum =  sum + ln2[l];
+					if (ln2f[l] <= b){
+						sum =  sum + ln2f[l];
 					}
 					else{
-					}
-					if (sum%d==0){
-					//	print(sum-1//d)
-					}
-					else{
-						//print(sum//d)
 					}
 				}
+				if (sum%d==0){
+					printf("Sum is: %d\n", (sum-1)/d);
+				}
+				else{
+					printf("Sum is: %d\n", sum/d);
+				}
+				
+				
+			}
+			else{
 			}
 		}
-    else{
-      break;
+		else{
+			break;
 		}
     cl +=1;
 	}
@@ -103,11 +108,11 @@ int main(){
 	scanf("%d,%d,%d.", &n,&b,&d);
 	printf("n: %d | b: %d | d: %d\n", n,b,d); 
 	printf("Enter orange sizes (Must be less than 10). Seperate with a comma, don't put a '.':\n");
-	char* ln2f[]={"","uu",""};
+	int ln2f[]={1,2,3};
 	splitter(n, &j, ln2f);
 	printf("j-final: %d\n", j);
-	printf("%s\n", ln2f[1]);
-	//Discriminant(j, n, b, d, sum, *ln2);
+	printf("%d\n", ln2f[1]);
+	Discriminant(j, n, b, d, sum, ln2f);
 	return 0;
 	/*for(i=0, i<sizeof(ln2)+1, i++){
 	//printf("Final %d: %s\n", i+1, ln2[i])
